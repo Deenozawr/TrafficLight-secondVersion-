85867e192a78e33732f6addcd0e17c8d1cdd7f5e
Deniss Akisins <Denjaa3@icloud.com>
2023-04-20 23:34:52 +0300

    Setup three "Views" and one "Button"

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var grennLightView: UIView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
